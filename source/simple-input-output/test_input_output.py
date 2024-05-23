87d68b4e131cdd44bc0f1a52469897e961c30ea5
TrucDev <truc.huynh@austincc.edu>
2024-05-23 11:12:03 -0500

    Update test_input_output.py
    update the comment

--- source/simple-input-output/test_input_output.py
+++ source/simple-input-output/test_input_output.py
@@ -1,8 +1,14 @@
 import unittest
 from input_output import process_input
 
+"""
+This module contains unit tests for the input_output module.
+"""
 
 class TestMain(unittest.TestCase):
+    """
+    TestMain is a test case class for testing the process_input function.
+    """
     def test_process_input(self):
         """
         Test the process_input function.
@@ -10,7 +16,6 @@ class TestMain(unittest.TestCase):
         self.assertEqual(process_input("test"), "You entered: test")
         self.assertEqual(process_input("123"), "You entered: 123")
         self.assertEqual(process_input(""), "You entered: ")
-
 
 if __name__ == '__main__':
     unittest.main()
